452ede361ff53e70e24ddb85a0a3fa81603613d3
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-07-30 21:06:55 +0800

    fix: preflight

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,7 +12,7 @@ var AllowArr = util.CreateConfig("gin").GetStringSlice("allowOrigins")
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     AllowArr,                                                                                     // 允许所有源
+		//AllowOrigins:     AllowArr,                                                                                     // 允许所有源
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},                                 // 允许的 HTTP 方法
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Auth_token", "Authorization", "Refresh_token"}, // 允许的请求头
 		ExposeHeaders:    []string{"Content-Length"},                                                                   // 允许暴露给浏览器的响应头
