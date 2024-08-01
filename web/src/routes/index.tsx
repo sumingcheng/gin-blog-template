151a2b1d89ef8d7c548b0a54bfb62a57a479c014
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-08-01 14:49:30 +0800

    isLogin

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,11 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Home from '../pages/home/index.tsx';
 import Login from '../pages/login/index.tsx';
 import Blog from '../pages/blog/index.tsx'
 import HeaderLayout from '../components/headerLayout.tsx';
 import Edit from "../pages/blog/edit.tsx";
+import { useEffect } from "react";
 
 const AppRoutes = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('auth_token');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route element={ <HeaderLayout/> }>
